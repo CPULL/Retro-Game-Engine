82491e89daf7be0a8310ea3cd9d0b25dca9fbd79
CPU <claudio.pucci@gmail.com>
2021-01-26 22:22:40 +0100

    Started color normalization class

---
+++ Assets/Engine/Utilities/Col.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Col {
+
+  public static Color32 GetColor32(byte col) {
+    return Color.white;
+  }
+
+  public static Color32 GetColor32(byte col, byte intensity) {
+    return Color.white;
+  }
+
+  public static Color32 GetColor32(int r, int g, int b, int a=255) {
+    return Color.white;
+  }
+
+  public static Color32 GetColor32(int r, int g, int b, byte intensity, int a=255) {
+    return Color.white;
+  }
+
+  public static byte GetColorByte(Color32 col) {
+    return 0;
+  }
+
+  public static byte GetColorByte(Color32 col, byte intensity) {
+    return 0;
+  }
+
+}
