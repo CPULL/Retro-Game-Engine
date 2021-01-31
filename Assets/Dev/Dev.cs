da20abd93e4313c7ac35831d12d5e8dd74deab1e
CPU <claudio.pucci@gmail.com>
2021-01-31 13:03:05 +0100

    Started Rom editor

--- Assets/Dev/Dev.cs
+++ Assets/Dev/Dev.cs
@@ -10,70 +10,11 @@ public class Dev : MonoBehaviour {
   public MusicEditor musicEditor;
   public FontsEditor fontsEditor;
   public CodeEditor codeEditor;
+  public RomEditor romEditor;
   public Image[] Selection;
 
   private void Awake() {
     inst = this;
-  }
-
-  public void SpriteEditor() {
-    EmptyEditor.SetActive(false);
-    for (int i = 0; i < Selection.Length; i++)
-      Selection[i].enabled = i == 2;
-    spriteEditor.gameObject.SetActive(true);
-    waveformEditor.gameObject.SetActive(false);
-    tilemapEditor.gameObject.SetActive(false);
-    musicEditor.gameObject.SetActive(false);
-    fontsEditor.gameObject.SetActive(false);
-    codeEditor.gameObject.SetActive(false);
-  }
-
-  public void WaveformEditor() {
-    EmptyEditor.SetActive(false);
-    for (int i = 0; i < Selection.Length; i++)
-      Selection[i].enabled = i == 4;
-    spriteEditor.gameObject.SetActive(false);
-    waveformEditor.gameObject.SetActive(true);
-    tilemapEditor.gameObject.SetActive(false);
-    musicEditor.gameObject.SetActive(false);
-    fontsEditor.gameObject.SetActive(false);
-    codeEditor.gameObject.SetActive(false);
-  }
-
-  public void MusicEditor() {
-    EmptyEditor.SetActive(false);
-    for (int i = 0; i < Selection.Length; i++)
-      Selection[i].enabled = i == 5;
-    spriteEditor.gameObject.SetActive(false);
-    waveformEditor.gameObject.SetActive(false);
-    tilemapEditor.gameObject.SetActive(false);
-    musicEditor.gameObject.SetActive(true);
-    fontsEditor.gameObject.SetActive(false);
-    codeEditor.gameObject.SetActive(false);
-  }
-
-  public void TilemapEditor() {
-    for (int i = 0; i < Selection.Length; i++)
-      Selection[i].enabled = i == 3;
-    EmptyEditor.SetActive(false);
-    spriteEditor.gameObject.SetActive(false);
-    waveformEditor.gameObject.SetActive(false);
-    tilemapEditor.gameObject.SetActive(true);
-    musicEditor.gameObject.SetActive(false);
-    fontsEditor.gameObject.SetActive(false);
-    codeEditor.gameObject.SetActive(false);
-  }
-
-  public void FontsEditor() {
-    EmptyEditor.SetActive(false);
-    for (int i = 0; i < Selection.Length; i++)
-      Selection[i].enabled = i == 1;
-    spriteEditor.gameObject.SetActive(false);
-    waveformEditor.gameObject.SetActive(false);
-    tilemapEditor.gameObject.SetActive(false);
-    musicEditor.gameObject.SetActive(false);
-    fontsEditor.gameObject.SetActive(true);
-    codeEditor.gameObject.SetActive(false);
   }
 
   public void CodeEditor() {
@@ -86,6 +27,86 @@ public class Dev : MonoBehaviour {
     musicEditor.gameObject.SetActive(false);
     fontsEditor.gameObject.SetActive(false);
     codeEditor.gameObject.SetActive(true);
+    romEditor.gameObject.SetActive(false);
   }
+
+  public void RomEditor() {
+    EmptyEditor.SetActive(false);
+    for (int i = 0; i < Selection.Length; i++)
+      Selection[i].enabled = i == 1;
+    spriteEditor.gameObject.SetActive(false);
+    waveformEditor.gameObject.SetActive(false);
+    tilemapEditor.gameObject.SetActive(false);
+    musicEditor.gameObject.SetActive(false);
+    fontsEditor.gameObject.SetActive(false);
+    codeEditor.gameObject.SetActive(false);
+    romEditor.gameObject.SetActive(true);
+  }
+
+  public void FontsEditor() {
+    EmptyEditor.SetActive(false);
+    for (int i = 0; i < Selection.Length; i++)
+      Selection[i].enabled = i == 2;
+    spriteEditor.gameObject.SetActive(false);
+    waveformEditor.gameObject.SetActive(false);
+    tilemapEditor.gameObject.SetActive(false);
+    musicEditor.gameObject.SetActive(false);
+    fontsEditor.gameObject.SetActive(true);
+    codeEditor.gameObject.SetActive(false);
+    romEditor.gameObject.SetActive(false);
+  }
+
+  public void SpriteEditor() {
+    EmptyEditor.SetActive(false);
+    for (int i = 0; i < Selection.Length; i++)
+      Selection[i].enabled = i == 3;
+    spriteEditor.gameObject.SetActive(true);
+    waveformEditor.gameObject.SetActive(false);
+    tilemapEditor.gameObject.SetActive(false);
+    musicEditor.gameObject.SetActive(false);
+    fontsEditor.gameObject.SetActive(false);
+    codeEditor.gameObject.SetActive(false);
+    romEditor.gameObject.SetActive(false);
+  }
+
+  public void TilemapEditor() {
+    for (int i = 0; i < Selection.Length; i++)
+      Selection[i].enabled = i == 4;
+    EmptyEditor.SetActive(false);
+    spriteEditor.gameObject.SetActive(false);
+    waveformEditor.gameObject.SetActive(false);
+    tilemapEditor.gameObject.SetActive(true);
+    musicEditor.gameObject.SetActive(false);
+    fontsEditor.gameObject.SetActive(false);
+    codeEditor.gameObject.SetActive(false);
+    romEditor.gameObject.SetActive(false);
+  }
+
+  public void WaveformEditor() {
+    EmptyEditor.SetActive(false);
+    for (int i = 0; i < Selection.Length; i++)
+      Selection[i].enabled = i == 5;
+    spriteEditor.gameObject.SetActive(false);
+    waveformEditor.gameObject.SetActive(true);
+    tilemapEditor.gameObject.SetActive(false);
+    musicEditor.gameObject.SetActive(false);
+    fontsEditor.gameObject.SetActive(false);
+    codeEditor.gameObject.SetActive(false);
+    romEditor.gameObject.SetActive(false);
+  }
+
+  public void MusicEditor() {
+    EmptyEditor.SetActive(false);
+    for (int i = 0; i < Selection.Length; i++)
+      Selection[i].enabled = i == 6;
+    spriteEditor.gameObject.SetActive(false);
+    waveformEditor.gameObject.SetActive(false);
+    tilemapEditor.gameObject.SetActive(false);
+    musicEditor.gameObject.SetActive(true);
+    fontsEditor.gameObject.SetActive(false);
+    codeEditor.gameObject.SetActive(false);
+  }
+
+
 }
 
