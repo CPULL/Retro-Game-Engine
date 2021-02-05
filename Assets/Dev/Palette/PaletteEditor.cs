752b632aa01111a5cfb3cb8ba4526b6b870af8d7
CPU <claudio.pucci@gmail.com>
2021-02-05 23:20:16 +0100

    Color picker like GIMP in Palette Editor

--- Assets/Dev/Palette/PaletteEditor.cs
+++ Assets/Dev/Palette/PaletteEditor.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PaletteEditor : MonoBehaviour {
-  Pixel[] pixels = new Pixel[256];
+  readonly Pixel[] pixels = new Pixel[256];
   public Transform PaletteContainer;
   public Slider HueSlider;
   public Slider RSlider;
@@ -18,6 +18,8 @@ public class PaletteEditor : MonoBehaviour {
   public RawImage SelectedColor;
   public RawImage ColorPicker;
   Texture2D ColorPickerTexture;
+  public RectTransform ColorPickerH;
+  public RectTransform ColorPickerV;
 
   void Start() {
     int pos = 0;
@@ -27,15 +29,65 @@ public class PaletteEditor : MonoBehaviour {
       pos++;
     }
 
+    RSlider.SetValueWithoutNotify(255);
+    GSlider.SetValueWithoutNotify(255);
+    BSlider.SetValueWithoutNotify(255);
+    ASlider.SetValueWithoutNotify(255);
+    SetColor();
+  }
+
+  public void SetColor() {
+    byte r = (byte)RSlider.value;
+    byte g = (byte)GSlider.value;
+    byte b = (byte)BSlider.value;
+    byte a = (byte)ASlider.value;
+    SelectedColor.color = new Color32(r, g, b, a);
+    RVal.text = r.ToString();
+    GVal.text = g.ToString();
+    BVal.text = b.ToString();
+    AVal.text = a.ToString();
+    Color.RGBToHSV(SelectedColor.color, out float h, out float s, out float v);
+    HueSlider.SetValueWithoutNotify(h * 360);
+    ColorPickerV.anchoredPosition = new Vector2(s * 256, 0);
+    ColorPickerH.anchoredPosition = new Vector2(0, -256 * (1 - v));
     ColorPickerTexture = new Texture2D(256, 256, TextureFormat.RGB24, false);
     for (int y = 255; y >= 0; y--)
       for (int x = 0; x < 255; x++)
-        ColorPickerTexture.SetPixel(x, y, new Color32((byte)x, (byte)y, 0, 255));
+        ColorPickerTexture.SetPixel(
+          x, y,
+          Color.HSVToRGB(h, x / 255f, y / 255f));
     ColorPickerTexture.Apply();
     ColorPicker.texture = ColorPickerTexture;
   }
 
-  // Update is called once per frame
+  public void SetColorHSV() {
+    float h = HueSlider.value / 360f;
+    Color.RGBToHSV(SelectedColor.color, out _, out float s, out float v);
+    Color c = Color.HSVToRGB(h, s, v);
+    byte r = (byte)(c.r * 255);
+    byte g = (byte)(c.g * 255);
+    byte b = (byte)(c.b * 255);
+    RSlider.SetValueWithoutNotify(r);
+    GSlider.SetValueWithoutNotify(g);
+    BSlider.SetValueWithoutNotify(b);
+    byte a = (byte)ASlider.value;
+    SelectedColor.color = new Color32(r, g, b, a);
+    RVal.text = r.ToString();
+    GVal.text = g.ToString();
+    BVal.text = b.ToString();
+    AVal.text = a.ToString();
+    ColorPickerV.anchoredPosition = new Vector2(s * 256, 0);
+    ColorPickerH.anchoredPosition = new Vector2(0, -256 * (1 - v));
+    ColorPickerTexture = new Texture2D(256, 256, TextureFormat.RGB24, false);
+    for (int y = 255; y >= 0; y--)
+      for (int x = 0; x < 255; x++)
+        ColorPickerTexture.SetPixel(
+          x, y,
+          Color.HSVToRGB(h, x / 255f, y / 255f));
+    ColorPickerTexture.Apply();
+    ColorPicker.texture = ColorPickerTexture;
+  }
+
   void Update() {
 
   }
@@ -44,6 +96,41 @@ public class PaletteEditor : MonoBehaviour {
     Debug.Log(p.pos);
   }
 
+
+  void Rgb2Hsl(byte rb, byte gb, byte bb, out int hue, byte sat, byte lum) {
+    // convert r,g,b [0,255] range to [0,1]
+    float r = rb / 255f;
+    float g = gb / 255f;
+    float b = bb / 255f;
+    // get the min and max of r,g,b
+    float max = r > g ? r : g;
+    max = max > b ? max : b;
+    float min  = r < g ? r : g;
+    min = min < b ? min : b;
+
+    // lightness is the average of the largest and smallest color components
+    lum = (byte)((max + min) * 127.999f);
+    if (max == min) { // no saturation
+      hue = 0;
+      sat = 0;
+    }
+    else {
+      var c = max - min; // chroma
+                         // saturation is simply the chroma scaled to fill
+                         // the interval [0, 1] for every combination of hue and lightness
+      sat = (byte)(255 * (c / (1 - Mathf.Abs(2 * (lum / 255) - 1))));
+      hue = 0;
+      if (max == r) {
+        hue = (int)(360 * (g - b) / c);
+      }
+      if (max == g) {
+        hue = (int)(360 * ((b - r) / c + 2));
+      }
+      if (max == b) {
+        hue = (int)(360 * ((r - g) / c + 4));
+      }
+    }
+  }
 }
 
 /*
