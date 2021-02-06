a954548dd938c1fc63a9df0b9541e047ad214af7
CPU <claudio.pucci@gmail.com>
2021-02-06 12:34:50 +0100

    Color picker in palette editor completed

---
+++ Assets/Dev/Palette/ColorPicker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ColorPicker : MonoBehaviour, IPointerClickHandler {
+  public RectTransform ColorPickerH;
+  public RectTransform ColorPickerV;
+  public PaletteEditor editor;
+
+  public void OnPointerClick(PointerEventData ed) {
+    float s = (ed.position.x - 1343) / 255f;
+    float v = (ed.position.y - 272) / -255f;
+    int x = (int)(s * 255 -.5f);
+    int y = (int)(v * -255 +.5f);
+    ColorPickerV.anchoredPosition = new Vector2(x, 0);
+    ColorPickerH.anchoredPosition = new Vector2(0, y);
+
+    editor.SetColorSV(s, 1 - v);
+  }
+}
--- Assets/Dev/Palette/PaletteEditor.cs
+++ Assets/Dev/Palette/PaletteEditor.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -52,7 +53,75 @@ public class PaletteEditor : MonoBehaviour {
     ColorPickerH.anchoredPosition = new Vector2(0, -256 * (1 - v));
     ColorPickerTexture = new Texture2D(256, 256, TextureFormat.RGB24, false);
     for (int y = 255; y >= 0; y--)
-      for (int x = 0; x < 255; x++)
+      for (int x = 0; x < 256; x++)
+        ColorPickerTexture.SetPixel(
+          x, y,
+          Color.HSVToRGB(h, x / 255f, y / 255f));
+    ColorPickerTexture.Apply();
+    ColorPicker.texture = ColorPickerTexture;
+    if (a == 255)
+      HexColor.SetTextWithoutNotify(r.ToString("X2") + g.ToString("X2") + g.ToString("X2"));
+    else
+      HexColor.SetTextWithoutNotify(r.ToString("X2") + g.ToString("X2") + g.ToString("X2") + a.ToString("X2"));
+  }
+
+  readonly Regex rghex = new Regex("[0-9a-f]{3,8}", RegexOptions.IgnoreCase, System.TimeSpan.FromSeconds(1));
+  public void SetColorHex() {
+    string hex = HexColor.text.Trim();
+    if ((hex.Length != 3 && hex.Length != 4 && hex.Length != 6 && hex.Length != 8) || !rghex.IsMatch(hex)) {
+      uint hx = (uint)(((byte)RSlider.value << 24) + ((byte)GSlider.value << 16) + ((byte)BSlider.value << 8) + ((byte)ASlider.value));
+      HexColor.SetTextWithoutNotify(hx.ToString("x4"));
+      return;
+    }
+    byte r=0, g=0, b=0, a=255;
+    if (hex.Length == 3) {
+      r = (byte)System.Convert.ToInt32(hex.Substring(0, 1), 16);
+      g = (byte)System.Convert.ToInt32(hex.Substring(1, 1), 16);
+      b = (byte)System.Convert.ToInt32(hex.Substring(2, 1), 16);
+      r *= 0x11;
+      g *= 0x11;
+      b *= 0x11;
+      a = (byte)ASlider.value;
+    }
+    else if (hex.Length == 4) {
+      r = (byte)System.Convert.ToInt32(hex.Substring(0, 1), 16);
+      g = (byte)System.Convert.ToInt32(hex.Substring(1, 1), 16);
+      b = (byte)System.Convert.ToInt32(hex.Substring(2, 1), 16);
+      a = (byte)System.Convert.ToInt32(hex.Substring(3, 1), 16);
+      r *= 0x11;
+      g *= 0x11;
+      b *= 0x11;
+      a *= 0x11;
+    }
+    else if (hex.Length == 6) {
+      r = (byte)System.Convert.ToInt32(hex.Substring(0, 2), 16);
+      g = (byte)System.Convert.ToInt32(hex.Substring(2, 2), 16);
+      b = (byte)System.Convert.ToInt32(hex.Substring(4, 2), 16);
+      a = (byte)ASlider.value;
+    }
+    else if (hex.Length == 8) {
+      r = (byte)System.Convert.ToInt32(hex.Substring(0, 2), 16);
+      g = (byte)System.Convert.ToInt32(hex.Substring(2, 2), 16);
+      b = (byte)System.Convert.ToInt32(hex.Substring(4, 2), 16);
+      a = (byte)System.Convert.ToInt32(hex.Substring(6, 2), 16);
+    }
+
+    RSlider.SetValueWithoutNotify(r);
+    GSlider.SetValueWithoutNotify(g);
+    BSlider.SetValueWithoutNotify(b);
+    ASlider.SetValueWithoutNotify(a);
+    SelectedColor.color = new Color32(r, g, b, a);
+    RVal.text = r.ToString();
+    GVal.text = g.ToString();
+    BVal.text = b.ToString();
+    AVal.text = a.ToString();
+    Color.RGBToHSV(SelectedColor.color, out float h, out float s, out float v);
+    HueSlider.SetValueWithoutNotify(h * 360);
+    ColorPickerV.anchoredPosition = new Vector2(s * 256, 0);
+    ColorPickerH.anchoredPosition = new Vector2(0, -256 * (1 - v));
+    ColorPickerTexture = new Texture2D(256, 256, TextureFormat.RGB24, false);
+    for (int y = 255; y >= 0; y--)
+      for (int x = 0; x < 256; x++)
         ColorPickerTexture.SetPixel(
           x, y,
           Color.HSVToRGB(h, x / 255f, y / 255f));
@@ -60,7 +129,7 @@ public class PaletteEditor : MonoBehaviour {
     ColorPicker.texture = ColorPickerTexture;
   }
 
-  public void SetColorHSV() {
+  public void SetColorH() {
     float h = HueSlider.value / 360f;
     Color.RGBToHSV(SelectedColor.color, out _, out float s, out float v);
     Color c = Color.HSVToRGB(h, s, v);
@@ -80,12 +149,45 @@ public class PaletteEditor : MonoBehaviour {
     ColorPickerH.anchoredPosition = new Vector2(0, -256 * (1 - v));
     ColorPickerTexture = new Texture2D(256, 256, TextureFormat.RGB24, false);
     for (int y = 255; y >= 0; y--)
-      for (int x = 0; x < 255; x++)
+      for (int x = 0; x < 256; x++)
         ColorPickerTexture.SetPixel(
           x, y,
           Color.HSVToRGB(h, x / 255f, y / 255f));
     ColorPickerTexture.Apply();
     ColorPicker.texture = ColorPickerTexture;
+    if (a == 255)
+      HexColor.SetTextWithoutNotify(r.ToString("X2") + g.ToString("X2") + g.ToString("X2"));
+    else
+      HexColor.SetTextWithoutNotify(r.ToString("X2") + g.ToString("X2") + g.ToString("X2") + a.ToString("X2"));
+  }
+
+  public void SetColorSV(float s, float v) {
+    float h = HueSlider.value / 360f;
+    Color c = Color.HSVToRGB(h, s, v);
+    byte r = (byte)(c.r * 255);
+    byte g = (byte)(c.g * 255);
+    byte b = (byte)(c.b * 255);
+    RSlider.SetValueWithoutNotify(r);
+    GSlider.SetValueWithoutNotify(g);
+    BSlider.SetValueWithoutNotify(b);
+    byte a = (byte)ASlider.value;
+    SelectedColor.color = new Color32(r, g, b, a);
+    RVal.text = r.ToString();
+    GVal.text = g.ToString();
+    BVal.text = b.ToString();
+    AVal.text = a.ToString();
+    ColorPickerTexture = new Texture2D(256, 256, TextureFormat.RGB24, false);
+    for (int y = 255; y >= 0; y--)
+      for (int x = 0; x < 256; x++)
+        ColorPickerTexture.SetPixel(
+          x, y,
+          Color.HSVToRGB(h, x / 255f, y / 255f));
+    ColorPickerTexture.Apply();
+    ColorPicker.texture = ColorPickerTexture;
+    if (a == 255)
+      HexColor.SetTextWithoutNotify(r.ToString("X2") + g.ToString("X2") + g.ToString("X2"));
+    else
+      HexColor.SetTextWithoutNotify(r.ToString("X2") + g.ToString("X2") + g.ToString("X2") + a.ToString("X2"));
   }
 
   void Update() {
