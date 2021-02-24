f418b94cce0f21c130f067d2a84c049b8077b0b7
CPU <claudio.pucci@gmail.com>
2021-02-24 09:53:11 +0100

    Removed the old CodeLine and restored the deleted Comment class

---
+++ Assets/Dev/Code/Comment.cs
@@ -0,0 +1,25 @@
+﻿
+public class Comment {
+  public string comment;
+  public CodeNode.CommentType type;
+  public int indent;
+  public Comment() {
+    comment = null;
+    type = CodeNode.CommentType.None;
+    indent = 0;
+  }
+  public Comment(string c, CodeNode.CommentType t) {
+    comment = c;
+    type = t;
+    indent = 0;
+  }
+  internal void Set(string value, CodeNode.CommentType t) {
+    comment = value;
+    type = t;
+  }
+  public void Zero() {
+    comment = null;
+    type = CodeNode.CommentType.None;
+    indent = 0;
+  }
+}
