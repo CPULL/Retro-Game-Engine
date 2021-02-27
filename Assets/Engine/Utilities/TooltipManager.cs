ab021c1b53e2ae7fc596e29a613d3e796e95a941
CPU <claudio.pucci@gmail.com>
2021-02-27 21:16:37 +0100

    Improved tooltips placement

--- Assets/Engine/Utilities/TooltipManager.cs
+++ Assets/Engine/Utilities/TooltipManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TooltipManager : MonoBehaviour {
   static TooltipManager inst;
@@ -8,6 +9,7 @@ public class TooltipManager : MonoBehaviour {
   public RectTransform RT;
   public TextMeshProUGUI Msg;
   string text = null;
+  bool visible = false;
 
   void Awake() {
     inst = this;
@@ -19,7 +21,7 @@ public class TooltipManager : MonoBehaviour {
     Vector2 pos = Input.mousePosition;
     pos.y = -1080 + pos.y + 32;
     pos.x += 32;
-    if (pos.x > 1700) pos.x = 1700;
+    if (pos.x > 1650) pos.x = 1650;
     if (pos.y > 0) pos.y = 0;
     inst.RT.anchoredPosition = pos;
     inst.Msg.text = txt;
@@ -27,6 +29,7 @@ public class TooltipManager : MonoBehaviour {
       inst.StopCoroutine(inst.hidingCoroutine);
       inst.hidingCoroutine = null;
     }
+    inst.visible = true;
   }
 
   public static void Hide(string txt) {
@@ -45,5 +48,17 @@ public class TooltipManager : MonoBehaviour {
     }
     inst.Container.SetActive(false);
     hidingCoroutine = null;
+    visible = false;
+  }
+
+  private void Update() {
+    if (!visible) return;
+
+    Vector2 pos = RT.anchoredPosition;
+    float y = Input.mousePosition.y - pos.y - 1080 + 32;
+    if (0 < y && pos.y < 80) {
+      pos.y += y;
+      RT.anchoredPosition = pos;
+    }
   }
 }
