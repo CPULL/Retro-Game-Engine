a24a4932c5389d8e44246d8d7d40ceba56cafa91
CPU <claudio.pucci@gmail.com>
2021-03-23 15:57:39 +0100

    Fixed a problem on tooltips moving too much

--- Assets/Engine/Utilities/TooltipManager.cs
+++ Assets/Engine/Utilities/TooltipManager.cs
@@ -10,20 +10,24 @@ public class TooltipManager : MonoBehaviour {
   public TextMeshProUGUI Msg;
   string text = null;
   bool visible = false;
+  Vector2 originalPos = Vector2.zero;
 
   void Awake() {
     inst = this;
   }
 
   public static void Show(string txt) {
+    if (inst.text == txt) return;
     inst.Container.SetActive(true);
     inst.text = txt;
     Vector2 pos = Input.mousePosition;
     pos.y = -1080 + pos.y + 32;
-    pos.x += 32;
+    if (pos.x > 960) pos.x -= 32;
+    else pos.x += 32;
     if (pos.x > 1650) pos.x = 1650;
     if (pos.y > 0) pos.y = 0;
     inst.RT.anchoredPosition = pos;
+    inst.originalPos = pos;
     inst.Msg.text = txt;
     if (inst.hidingCoroutine != null) {
       inst.StopCoroutine(inst.hidingCoroutine);
@@ -34,7 +38,7 @@ public class TooltipManager : MonoBehaviour {
 
   public static void Hide(string txt) {
     if (inst.text != txt) return;
-    inst.timeForHiding = .5f;
+    inst.timeForHiding = 5.5f; // FIXME
     if (inst.hidingCoroutine == null) inst.hidingCoroutine = inst.StartCoroutine(inst.HideDelayed());
   }
 
@@ -54,10 +58,11 @@ public class TooltipManager : MonoBehaviour {
   private void Update() {
     if (!visible) return;
 
-    Vector2 pos = RT.anchoredPosition;
+    Vector2 pos = originalPos;
     float y = Input.mousePosition.y - pos.y - 1080 + 32;
     if (0 < y && pos.y < 80) {
-      pos.y += y;
+      if (Input.mousePosition.x > 960) pos.x -= 180;
+      else pos.x += 180;
       RT.anchoredPosition = pos;
     }
   }
